f144d461a2f7cc6c8cc893ae61647c37510629d1
max <max@draminski.com>
2024-11-10 18:23:34 +0100

    update cmake

--- src/build/README.cmake
+++
@@ -1,9 +0,0 @@
-Notes for cmake.
-
-% cd QProgressIndicator/src/build
-% cmake -DCMAKE_BUILD_TYPE=Debug ..
-% cmake -DCMAKE_BUILD_TYPE=Release ..
-% make clean
-% make Example
-% sudo make install
-
